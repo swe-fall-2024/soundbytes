eec5a0d10975e74023e33a16a49d7f57bfbab2df
krhartley <314katherine@gmail.com>
2025-02-09 12:32:53 -0500

    profile update and friend profile addition

--- angular-frontend/src/app/views/friend-profile/friend-profile.component.ts
+++ angular-frontend/src/app/views/friend-profile/friend-profile.component.ts
@@ -2,7 +2,6 @@ import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import {MatChipsModule} from '@angular/material/chips';
-import {MatProgressBarModule} from '@angular/material/progress-bar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatGridListModule} from '@angular/material/grid-list';
 import {MatListModule} from '@angular/material/list';
@@ -10,12 +9,12 @@ import {MatDividerModule} from '@angular/material/divider';
 import {MatToolbarModule} from '@angular/material/toolbar';
 
 @Component({
-  selector: 'app-profile',
-  imports: [MatToolbarModule, MatDividerModule, MatListModule, MatCardModule, MatButtonModule, MatChipsModule, MatProgressBarModule, MatIconModule, MatGridListModule],
-  templateUrl: './profile.component.html',
-  styleUrl: './profile.component.css',
+  selector: 'app-friend-profile',
+  imports: [MatToolbarModule, MatDividerModule, MatListModule, MatCardModule, MatButtonModule, MatChipsModule, MatIconModule, MatGridListModule],
+  templateUrl: './friend-profile.component.html',
+  styleUrl: './friend-profile.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ProfileComponent {
-  
+export class FriendProfileComponent {
+
 }
