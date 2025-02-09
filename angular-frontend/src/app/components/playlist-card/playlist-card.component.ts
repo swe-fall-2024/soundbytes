7a4d17486e7c74b9f88b61f2315f5e19232367d1
MaryHanson11 <maryhanson@ufl.edu>
2025-02-09 17:11:52 -0500

    basic feed component done. added dummy navbar

---
+++ angular-frontend/src/app/components/playlist-card/playlist-card.component.ts
@@ -0,0 +1,52 @@
+import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+
+
+@Component({
+  selector: 'app-playlist-card',
+    template: `
+      <mat-card class="example-card" appearance="outlined">
+        <mat-card-header>
+          <mat-card-title>{{post.title}}</mat-card-title>
+          <mat-card-subtitle>{{post.user}}</mat-card-subtitle>
+          <div mat-card-avatar class="example-header-image"></div>
+        </mat-card-header>
+        <mat-card-content>
+        <div>
+          <mat-card appearance="outlined" class="song-player">
+            <mat-card-content class="content">
+              {{post.content.playlist_title}}
+              <a href={{post.content.playlist_url}} target="_blank"  mat-button>
+              <span class="material-icons">play_circle</span>
+              </a>
+            </mat-card-content>
+          </mat-card>
+        </div>
+        </mat-card-content>
+        <mat-card-actions>
+          <button mat-button>LIKE</button>
+          <button mat-button>SHARE</button>
+        </mat-card-actions>
+      </mat-card>
+    `,
+    styleUrl: './playlist-card.component.css',
+    imports: [MatCardModule, MatButtonModule],
+    changeDetection: ChangeDetectionStrategy.OnPush,
+  })
+  export class PlaylistCardComponentComponent {
+    constructor() {
+      console.log('myCustomComponent');
+    }
+    @Input() post: any;
+  }
+
+  /*
+  embedded:
+  <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/1yJb4XCnM4KfeO2UkMAYnp?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+  non-embeded content:
+  {{post.content.playlist_title}}
+  <a href={{post.content.playlist_url}} target="_blank"  mat-button>
+  <span class="material-icons">play_circle</span>
+  </a>
+  */
